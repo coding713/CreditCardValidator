571038bf0626536bed043eb6855126034f844b70
crimify <crimifycrim@yahoo.com>
2021-03-13 22:49:26 -0500

    NewCode

---
+++ CreditCard.py
@@ -0,0 +1,34 @@
+
+
+
+while 1 == 1:
+    cnlist = input("What is your credit card number? ")
+    if cnlist.isdigit() and len(cnlist) == 16:
+        cn = [int(x) for x in cnlist]
+
+#def card_validator:
+        sumx = 0
+        list_1 = cn[0::2]
+        for num in list_1:
+            x = num * 2
+            if x >= 10:
+                x = (x//10 + x%10)
+            sumx = sumx + x
+
+
+        sumy = 0
+        list_2 = cn[1::2]
+        for num in list_2:
+            y = num * 1
+            sumy = sumy + y
+
+        sumall = sumx+sumy
+        if sumall%10 == 0:
+            print("It is valid")
+            break
+        else:
+            print("It is invalid, please try again or enter 'No'")
+    elif(cnlist == "No"):
+        break
+    else:
+        print("Please enter a valid 16 digit credit card number!")
